96e7add01ba6f50aabbcf227c59ef369d558b4ee
rodrimr16 <rodrimaaare@gmail.com>
2025-04-10 13:53:03 +0200

    Error: cambio en la lógica de la función suma

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,7 @@
+package src;
+
 public class Calculadora {
     public static int suma(int a, int b) {
-        return a + b;
+        return a - b;  // Error: la función de suma ahora hace una resta
     }
 }
